faadb50f054cd72fe5c88048399457139e0d4765
jendermine <jendermine@gmail.com>
2025-12-29 00:26:08 +0530

    Revert "sky: Import soong namespace from hardware/qcom-caf/sm8450/audio/agm/ipc/HwBinders/legacy"
    This reverts commit 0692c2055dce86de41ffc8b4f030690d2bb3ddf0.
    
    Change-Id: I464e7198f693c10eb64511216400c7d47a402170

--- device.mk
+++ device.mk
@@ -428,8 +428,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/xiaomi \
     hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/qcom-caf/sm8450/audio/agm/ipc/HwBinders/legacy
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
